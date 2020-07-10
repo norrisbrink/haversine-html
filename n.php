5a1465d40fad562918d4a8291eaf6ae898a84484
norrisbrink <norrisbrink@outlook.com>
2020-07-10 14:49:52 +0900

    first commit

---
+++ n.php
@@ -0,0 +1,34 @@
+<?php
+//arrive, sejong gov building
+ 
+$lat1= '126.978304';
+$lon1= '37.574240';
+$lat2= '127.289030'; //세종시청
+$lon2= '36.480146';
+
+//$lat2= '127.267749'; //정부세종청사
+//$lon2= '36.504337';
+$url = "https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?start=$lat1,$lon1&goal=$lat2,$lon2&option=trafast";
+$headers = array("X-NCP-APIGW-API-KEY-ID: 4x7btpvjs3", "X-NCP-APIGW-API-KEY: V9kKAURy62T92qvdgXyWcsz11snsncFeCDqMlfTl");
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  // exec 실행 후 결과 자동으로 출력 안됨
+
+//print_r($url);
+
+$result = curl_exec($ch);
+
+print_r($result);
+
+if ($result === false) {
+    //print_r(curl_errno($ch));
+    //echo "<br>";
+    print_r(curl_error($ch));
+    echo "<br>";
+}
+
+curl_close($ch);
+?>
+hi
